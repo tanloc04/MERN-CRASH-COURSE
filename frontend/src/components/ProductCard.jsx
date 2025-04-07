43d752285e1727874d7989da4c06263292b173d5
tanloc04 <tanloc040403@gmail.com>
2025-04-07 18:22:43 +0700

    lan thu 2 dua code len server

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,10 +1,69 @@
-import { Box, Image, Heading, Text, HStack, IconButton, useColorModeValue} from '@chakra-ui/react'
-import React from 'react'
+import { Box, Image, Heading, Text, HStack, IconButton, useColorModeValue, useToast, VStack, useDisclosure, Input, Button} from '@chakra-ui/react'
+import React, { useState } from 'react'
 import { EditIcon, DeleteIcon} from '@chakra-ui/icons'
+import { useProductStore } from '../store/product'
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    ModalCloseButton,
+  } from '@chakra-ui/react'
 
 const ProductCard = ({product}) => {
+    const [updatedProduct, setUpdatedProduct] = useState(product);
     const textColor = useColorModeValue("gray.600", "gray.200");
     const bg = useColorModeValue("white", "gray.800");
+    const {deleteProduct, updateProduct} = useProductStore();
+    const toast = useToast();
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    const handleDeleteProduct = async (pid) => {
+        const {success, message} = await deleteProduct(pid)
+        if(!success){
+            toast({
+                title: 'Error',
+                description: message,
+                status: 'error',
+                duration: 3000,
+                isClosable: true,
+            })
+        }
+        else{
+            toast({
+                title: 'Success',
+                description: message,
+                status: 'success',
+                duration: 3000,
+                isClosable: true,
+            })
+        }
+    };
+
+    const handleUpdateProduct = async (pid, updatedProduct) =>{
+        const {success, message} = await updateProduct(pid, updatedProduct);
+        onClose();
+
+        if (!success){
+            toast({
+                title: "Error",
+                description: message,
+                status: "error",
+                duration: 3000,
+                isClosable: true
+            });}
+            else{
+                toast({
+                    title: "Success",
+                    description: "Product Updated Successfully!",
+                    status: "success",
+                    duration: 3000,
+                    isClosable: true
+                })
+            }
+        };
   return (
     <Box
         shadow={'lg'}
@@ -24,14 +83,33 @@ const ProductCard = ({product}) => {
             </Text>
 
             <HStack spacing={2}>
-                <IconButton icon={<EditIcon />} colorScheme='blue'/>
-                <IconButton icon={<DeleteIcon />} colorScheme='red' />
+                <IconButton icon={<EditIcon />} colorScheme='blue' onClick={onOpen}/>
+                <IconButton icon={<DeleteIcon />} onClick={( )=>handleDeleteProduct(product._id)} colorScheme='red' />
             </HStack>
 
         </Box>
 
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
 
-        
+                <ModalContent>
+                    <ModalHeader>
+                        Update Product
+                    </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <VStack spacing={4}>
+                            <Input placeholder ='Product Name' name ='name' value={updatedProduct.name} onChange={(e) => setUpdatedProduct({...updatedProduct, name: e.target.value})}/>
+                            <Input placeholder ='Product Price' name ='price' type = 'number' value={updatedProduct.price} onChange={(e) => setUpdatedProduct({...updatedProduct, price: e.target.value})}/>
+                            <Input placeholder ='URL Image' name ='image' value={updatedProduct.image} onChange={(e) => setUpdatedProduct({...updatedProduct, image: e.target.value})}/>
+                        </VStack>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={() => handleUpdateProduct(product._id, updatedProduct)}>Update</Button>
+                        <Button variant ='ghost' onClick={onClose}>Cancel</Button>
+                    </ModalFooter>
+                </ModalContent>
+        </Modal>      
     </Box>
   )
 };
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -31,7 +31,8 @@ const HomePage = () => {
           ))}
         </SimpleGrid>
 
-        <Text fontSize='xl' textAlign={"center"} fontWeight={"bold"} color={"gray.500"}>
+        {products.length === 0 && (
+          <Text fontSize='xl' textAlign={"center"} fontWeight={"bold"} color={"gray.500"}>
           No products found 😢 {" "}
           <Link to={"/create"} >
             <Text as='span' color={"blue.500"} _hover={{textDecoration: "underline"}}>
@@ -39,6 +40,7 @@ const HomePage = () => {
             </Text>
           </Link>
         </Text>
+        )}
 
       </VStack>
 
